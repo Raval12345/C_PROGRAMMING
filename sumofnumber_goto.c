45fc8cd5cd64161372c2066a4a1bcb07296f68f0
Raval12345 <148950920+Raval12345@users.noreply.github.com>
2023-11-03 17:46:07 +0530

    Add files via upload

---
+++ sumofnumber_goto.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+	int i=1,add=0;
+		sum :    add+=i; 
+	 
+	 i++;
+	if(i<=5)
+	{
+		
+		goto sum;
+	
+	}
+	else
+	{
+		printf("\n\n sum = %d",add);
+	}
+	 
+
+	         
+}
