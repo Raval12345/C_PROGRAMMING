b0f753437b10cca8a2915c0a594b9c9ee4ed3f3e
Raval12345 <148950920+Raval12345@users.noreply.github.com>
2023-11-06 00:14:57 +0530

    Add files via upload

---
+++ C_SumOfThreeNumberEnterByUser.cpp
@@ -0,0 +1,20 @@
+/*Sum Of Three Number Enter BY User*/
+#include<stdio.h>
+main()
+{
+	int a , b , c , sum;
+	
+	printf(" Enter Value Of A:- ");
+	scanf("%d",&a);
+	printf(" Enter Value of B:- ");
+	scanf("%d",&b);
+	printf(" Enter Value of C:- ");
+	scanf("%d",&c);
+	
+	sum=a+b+c;
+	
+	printf("Enter Value %d + %d + %d :- %d ",a,b,c,sum);
+	
+}
+
+
