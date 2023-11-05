cd50813eb9246133da7ed338b39d19765fde542b
Raval12345 <148950920+Raval12345@users.noreply.github.com>
2023-11-06 00:12:45 +0530

    Add files via upload

---
+++ C_MultiplicatioonOfTwoNumberEnterByUser.cpp
@@ -0,0 +1,18 @@
+/*Multiplication Of Two Number Enter By User*/
+
+#include<Stdio.h>
+main()
+{
+	int a , b , c ;
+	
+	printf("Enter Value Of A :- ");
+	scanf("%d",&a);
+	
+	printf("Enter Value Of B :- ");
+	scanf("%d",&b);
+	
+	c=a*b;
+	
+	printf("Multiplication Of %d And %d = %d ", a,b,c);
+	
+}
