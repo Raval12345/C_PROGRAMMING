9266c7cea0d499af68b67df1724230d2716ebfc3
Raval12345 <148950920+Raval12345@users.noreply.github.com>
2023-11-06 00:14:31 +0530

    Add files via upload

---
+++ C_SubstratationOfTwoNumberEnterByUser.cpp
@@ -0,0 +1,16 @@
+/*Substraction Of Two Number Enter By User*/
+
+#include<stdio.h>
+main()
+{
+	int a , b , c;
+	
+	printf(" Enter Value Of A :- ");
+	scanf("%d",&a);
+	printf(" Enter Value Of B :- ");
+	scanf("%d",&b);
+	
+	c=a-b;
+	
+	printf("Substraction Of %d And %d = %d",a,b,c);
+}
