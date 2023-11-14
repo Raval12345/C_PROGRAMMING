44617324f8b09a1134f3f91d4e6b3656b3779e3a
Raval12345 <148950920+Raval12345@users.noreply.github.com>
2023-11-14 16:06:21 +0530

    Add files via upload

---
+++ C_UserInputValue.cpp
@@ -0,0 +1,12 @@
+/*User Input Value*/
+
+#include<stdio.h>
+main()
+{
+	
+	int a;
+	printf("Enter Value: ");           /*For Ask User To Enter Value*/
+	scanf("%d",&a);                    /*Store Value In a*/
+	
+	printf("Enter Value Is %d ",a);    /* For Print User Enter Value*/
+}
