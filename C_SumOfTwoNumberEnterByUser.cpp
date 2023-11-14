81e3d44c1dbeaa4980ba1c12e380b8b240215d0b
Raval12345 <148950920+Raval12345@users.noreply.github.com>
2023-11-14 16:10:01 +0530

    Add files via upload

---
+++ C_SumOfTwoNumberEnterByUser.cpp
@@ -0,0 +1,17 @@
+/*Sum Of Two Value Enter By User*/
+
+#include<stdio.h>
+main()
+{
+	int a , b ,c ;
+	
+	printf("Enter Value Of A:- ");
+	scanf("%d",&a);
+	printf("\n");
+	printf("Enter Value Of B:- ");
+	scanf("%d",&b);
+	printf("\n");
+	c=a+b;
+	
+	printf("Sum Of %d And %d = %d \n",a,b,c);
+}
