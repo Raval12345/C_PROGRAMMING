49a635ac3cc29dedfd9f6f4bc5f5ce68a0336c44
Raval12345 <148950920+Raval12345@users.noreply.github.com>
2023-11-14 16:04:34 +0530

    Add files via upload

---
+++ C_DivisionOfTwoNumberEnterByUser.cpp
@@ -0,0 +1,18 @@
+/*Division Of Two Number Enter By User*/
+
+#include<stdio.h>
+main()
+{
+	int a , b , c ;
+	
+	printf("Enter Value Of A:- ");
+	scanf("%d",&a);
+	
+	printf("Enter Value of B:- ");
+	scanf("%d",&b);
+	
+	c=a/b;
+	
+	printf("Division Of %d And %d = %d ",a,b,c);
+	
+}
