3c2d3e9099f9a2bec088cb894e0aff0305657d00
Raval12345 <148950920+Raval12345@users.noreply.github.com>
2023-11-14 15:51:29 +0530

    Add files via upload

---
+++ C_AreaOfRectangle.cpp
@@ -0,0 +1,19 @@
+/*Area Of Rectangle*/
+
+#include<stdio.h>
+main()
+{
+	int Area , Length , Width ;
+	
+	printf("Enter The Length Of Rectangle : ");
+	scanf("%d",&Length);
+	
+	printf("Enter The Width Of Rectangle : ");
+	scanf("%d",&Width);
+	
+	Area=Length*Width;
+	
+	printf("Area Of Rectangle Is : %d",Area);
+	
+	
+}
